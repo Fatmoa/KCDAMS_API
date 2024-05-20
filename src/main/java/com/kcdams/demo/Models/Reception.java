8f306d71d463f0cffce8caad8f002d7c96daa2b4
Fatmoa <fatmamohd3654@gmail.com>
2024-05-20 19:19:10 +0300

    20/4/24 19:19

--- src/main/java/com/kcdams/demo/Models/Reception.java
+++ src/main/java/com/kcdams/demo/Models/Reception.java
@@ -1,4 +1,17 @@
 package com.kcdams.demo.Models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "reception")
+
 public class Reception {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String patCode;
+    private String patName;
+    @Lob
+    private byte [ ] picture;
 }
